d815c4962963af7d840b2c82ddcca55fdbea2eb9
Mahesh Kumar <mk799994@gmail.com>
2024-02-17 12:21:14 -0800

    initilized the project

---
+++ src/main/java/com/hack/cvcenter/CvCenterApplication.java
@@ -0,0 +1,13 @@
+package com.hack.cvcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CvCenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hack/cvcenter/CvCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hack.cvcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
