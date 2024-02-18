f18dcafe8aba459a19f934f7c9783e527120fc52
Mahesh Kumar <53092346+maheshhmahi@users.noreply.github.com>
2024-02-18 01:12:57 -0800

    Revert "REFACOTED: user experience api for handling many experience at once"

--- src/main/java/com/hack/cvcenter/dto/UserExperienceDto.java
+++ src/main/java/com/hack/cvcenter/dto/UserExperienceDto.java
@@ -1,10 +1,12 @@
 package com.hack.cvcenter.dto;
 
 import com.hack.cvcenter.constants.ErrorMessages;
+import com.hack.cvcenter.constants.ValidationRegex;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
-import java.util.Set;
+import java.time.LocalDate;
 
 @Data
 public class UserExperienceDto {
@@ -12,6 +14,27 @@ public class UserExperienceDto {
     @NotEmpty(message = ErrorMessages.IS_REQUIRED)
     private String userUuid;
 
-    private Set<ExperienceDto> experienceDetails;
+    @NotEmpty(message = ErrorMessages.IS_REQUIRED)
+    private String positionName;
+
+    @NotEmpty(message = ErrorMessages.IS_REQUIRED)
+    private String companyName;
+
+    @NotEmpty(message = ErrorMessages.IS_REQUIRED)
+    private String city;
+
+    @NotEmpty(message = ErrorMessages.IS_REQUIRED)
+    private String country;
+
+    @NotEmpty(message = ErrorMessages.IS_REQUIRED)
+    @Pattern(regexp = ValidationRegex.DATE_REGEX, message = ErrorMessages.DOB_ERR_MSG)
+    private String startDate;
+
+    @NotEmpty(message = ErrorMessages.IS_REQUIRED)
+    @Pattern(regexp = ValidationRegex.DATE_REGEX, message = ErrorMessages.DOB_ERR_MSG)
+    private String endDate;
+
+    @NotEmpty(message = ErrorMessages.IS_REQUIRED)
+    private String details;
 
 }
